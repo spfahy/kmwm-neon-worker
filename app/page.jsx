f7e90494e5a077d9175143a09ba6c0a066548c99
spfahy <spfahy@gmail.com>
2025-12-04 14:54:29 -0600

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>KMWM Dashboard</h1>
+      <p>Your dashboard is running. Use /goldcurve to view the metals curve.</p>
+    </div>
+  );
+}
